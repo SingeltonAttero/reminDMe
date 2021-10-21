515f879f9ad8e6028cf85725276b02c137134c1e
YWeber <yakowweberjavajob@gmail.com>
2021-10-21 02:54:13 +0200

    add proto data store app setting

--- app/src/main/java/ru/weber/remindme/ui/RemindMeViewModel.kt
+++ app/src/main/java/ru/weber/remindme/ui/RemindMeViewModel.kt
@@ -1,12 +1,8 @@
 package ru.weber.remindme.ui
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import ru.weber.remindme.domain.interactor.SettingInteractor
 import javax.inject.Inject
 
@@ -14,16 +10,7 @@ import javax.inject.Inject
 class RemindMeViewModel @Inject constructor(
     private val settingInteractor: SettingInteractor
 ) : ViewModel() {
-    private val themeAction: MutableStateFlow<Boolean> = MutableStateFlow(false)
-
     val themeState: Flow<Boolean>
-        get() = themeAction
+        get() = settingInteractor.getThemeSettingData()
 
-    init {
-        viewModelScope.launch {
-            settingInteractor.getThemeSettingData().collect {
-                themeAction.emit(it)
-            }
-        }
-    }
 }
